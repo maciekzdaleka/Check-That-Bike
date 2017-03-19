918667d6685db5944badd9ffe722163101898fe3
Maciej Piotrowski <c13470112@mydit.ie>
2017-03-19 14:03:24 +0000

    Implementing Theft Report (Date + Time Picker)

--- app/src/main/java/com/example/maciek/myapplication/Theft_details.java
+++ app/src/main/java/com/example/maciek/myapplication/Theft_details.java
@@ -1,11 +1,20 @@
 package com.example.maciek.myapplication;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class Theft_details extends AppCompatActivity {
 
@@ -28,11 +37,63 @@ public class Theft_details extends AppCompatActivity {
         DateEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //showTruitonTimePickerDialog(v);
-                //showTruitonDatePickerDialog(v);
+                showTruitonTimePickerDialog(v);
+                showTruitonDatePickerDialog(v);
             }
         });
 
+    }
+
+    public void showTruitonDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public static class DatePickerFragment extends DialogFragment implements
+            DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+            DateEdit.setText(day + "/" + (month + 1) + "/" + year);
+        }
+    }
+
+    public void showTruitonTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    public static class TimePickerFragment extends DialogFragment implements
+            TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            // Do something with the time chosen by the user
+            DateEdit.setText(DateEdit.getText() + " -" + hourOfDay + ":" + minute);
+        }
     }
 
     public boolean onKeyDown(int keyCode, KeyEvent event) {
